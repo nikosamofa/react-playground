567957b34dd6e78b3e5c3c6bd8fde56ceec9c15e
Nikos Amofa <nikosbright@gmail.com>
2020-12-11 02:20:07 -0500

    added  RouletteGun and Bomb components

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -1,15 +1,42 @@
 import React from 'react'
 
 class Bomb extends React.Component {
-    state = { 
+    state = {
         count: 0
-     }
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.setState({
+                count: this.state.count + 1
+            })
+        }, 1000)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval)
+    }
+
+    renderDisplay (){
+        const {count}= this.state
+        if(count >=8){
+            clearInterval(this.interval)
+            return "BOOM!!";
+        }
+        else if (count %2 === 0){
+            return "Tick"
+        }
+
+        else{
+            return "tock"
+        }
+    }
 
 
     render() {
         return (
             <div className='Bomb'>
-                <p>" BOOM!!  "</p>
+                {this.renderDisplay()}
             </div>
         )
     }
--- src/state-drills/RouletteGun.js
+++ src/state-drills/RouletteGun.js
@@ -1,15 +1,52 @@
 import React from 'react';
 
-class RouletteGun extends React.Component{
-    constructor(props){
-        super(props)
-        this.state={}
+class RouletteGun extends React.Component {
+    static defaultProps = {
+        bulletInChamber: 6
+    }
+    state = {
+        chamber: null,
+        spinningTheChamber: false,
     }
 
-    render(){
+    componentWillUnmount() {
+        clearInterval(this.timeout)
+    }
+    handleClick = () => {
+        this.setState(
+            {
+                spinningTheChamber: true,
+            })
+
+        this.timeout = setTimeout(() => {
+            const randomChamber = Math.ceil(Math.random() * 6)
+            this.setState({
+                chamber: randomChamber,
+                spinningTheChamber: false,
+            })
+        }, 2000)
+    }
+
+    renderDisplay(){
+        const {chamber,spinningTheChamber}=this.state
+        const {bulletInChamber}= this.props
+        if (spinningTheChamber){
+            return "spinning the chamber and pulling the trigger!...."
+        }
+        else if (chamber===bulletInChamber){
+            return 'BANG!!!!!'
+        }
+        else{
+            return "you/'re Safe!"
+        }
+    }
+    render() {
         return (
-            <div>
-                <p>spinning the chamber and pulling the trigger! ...</p>
+            <div className='RouletteGun'>
+                <p>{this.renderDisplay()}</p>
+                <button onClick={this.handleClick}>
+                    Pull the trigger!
+                    </button>
             </div>
         )
     }
