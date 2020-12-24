1b378c7f8d87f87382fc2a0b749125927b442041
Nikos Amofa <nikosbright@gmail.com>
2020-12-23 19:46:54 -0500

    Added poem component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './paltrows-power-toes/App';
 import './index.css';
 import './state-drills/HelloWorld'
 import  './state-drills/Bomb'
 import './state-drills/RouletteGun';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom'
+
+
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
---
+++ src/paltrows-power-toes/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './src/paltrows-power-toes/App.js.js.js.js.js';
+
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
